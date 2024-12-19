ea364bfe8d9179c5371b0ceb12cba7c1d9904b94
Kacper Sulek <kacpersulek15022004@gmail.com>
2024-12-19 10:52:55 +0100

    test

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,19 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from "@angular/forms";
-import { Location, UpperCasePipe } from "@angular/common";
-import { Hero } from "../hero";
-import { ActivatedRoute } from "@angular/router";
-import { HeroService } from "../hero.service";
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
-  standalone: true,
-    imports: [
-        FormsModule,
-        UpperCasePipe
-    ],
   templateUrl: './hero-detail.component.html',
-  styleUrl: './hero-detail.component.css'
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero | undefined;
@@ -29,7 +24,7 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
@@ -39,7 +34,7 @@ export class HeroDetailComponent implements OnInit {
   }
 
   save(): void {
-    if(this.hero){
+    if (this.hero) {
       this.heroService.updateHero(this.hero)
         .subscribe(() => this.goBack());
     }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,46 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { NgFor } from "@angular/common";
-import { Hero } from "../hero";
-import { HeroService } from "../hero.service";
-import { RouterLink } from "@angular/router";
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
-  standalone: true,
-  imports: [
-    NgFor,
-    RouterLink
-  ],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService) { }
 
-  ngOnInit(){
+  ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => {
-      this.heroes = heroes;
-    });
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 
-  addHero(name: string): void {
+  add(name: string): void {
     name = name.trim();
-    if(!name) { return; }
-
+    if (!name) { return; }
     this.heroService.addHero({ name } as Hero)
       .subscribe(hero => {
         this.heroes.push(hero);
       });
   }
 
-  deleteHero(hero: Hero): void {
+  delete(hero: Hero): void {
     this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero.id).subscribe();
   }
+
 }
